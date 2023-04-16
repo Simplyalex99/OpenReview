9dd7ed141d5121048b4234f6eb28ab198defcc5d
Simplyalex99 <alexmflores99@hotmail.ca>
2023-04-15 20:13:49 -0400

    update: backend: caching time from 30 sec to 5 mins

--- backend/api/routes/businesses_routes.py
+++ backend/api/routes/businesses_routes.py
@@ -15,36 +15,36 @@ def autocomplete_route():
     return getAutocomplete()
 
 @app.route("/businesses/search", methods=["GET"])
-@cache.cached(timeout=30, query_string=True)
+@cache.cached(timeout=300, query_string=True)
 def index():
     return getBusinesses()
     
 @app.route("/businesses/<id>/reviews",methods=["GET"])
-@cache.cached(timeout=30, query_string=True)
+@cache.cached(timeout=300, query_string=True)
 def reviews_route(id):
     return getReviews(id)
 
 
 
 @app.route("/businesses/predictions/topics", methods=["POST"])
-@cache.cached(timeout=30)
+@cache.cached(timeout=300)
 def topics_route():
     return getTopics()
 
  
 @app.route("/businesses/recommendations/popular", methods=["GET"])
-@cache.cached(timeout=30, query_string=True)  
+@cache.cached(timeout=300, query_string=True)  
 def recommendations_popular_route():
     return getRecommendationsByPopularity()
 
 
 @app.route("/businesses/recommendations/categories", methods=["GET"])
-@cache.cached(timeout=30, query_string=True)
+@cache.cached(timeout=300, query_string=True)
 def recommendations_categories_route():
     return getRecommendationsByCategories()
 
 
 @app.route("/businesses/predictions/business-success", methods=["POST"])
-@cache.cached(timeout=30)
+@cache.cached(timeout=300)
 def predictions_route():
     return getPredictions()
