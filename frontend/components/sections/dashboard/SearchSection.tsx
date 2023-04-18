b04cce8d3d2b890e30e941180e9e47e6086bef78
Simplyalex99 <alexmflores99@hotmail.ca>
2023-04-18 09:37:38 -0400

    add: frontend: Search Section component

---
+++ frontend/components/sections/dashboard/SearchSection.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import dashboardStyles from '../../../styles/sections/Dashboard.module.scss';
+import { useAppSelector } from '../../../hooks';
+import { SearchBar } from '../../others/SearchBar';
+import { SearchSuggestions } from '../../others/SearchSuggestions';
+
+interface SearchSectionProps {
+  formInput: string;
+  suggestionsHandler: (title: string) => void;
+  inputHandler: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  searchHandler: () => void;
+  suggestions: string[];
+}
+export const SearchSection = ({
+  formInput,
+  suggestionsHandler,
+  inputHandler,
+  searchHandler,
+  suggestions,
+}: SearchSectionProps) => {
+  const stateTheme = useAppSelector((state) => state.themeReducer);
+  const { darkMode } = stateTheme;
+  return (
+    <>
+      <div className={`${dashboardStyles['searchbar-flex-container']}`}>
+        <SearchBar
+          input={formInput}
+          inputHandler={(e: React.ChangeEvent<HTMLInputElement>) =>
+            inputHandler(e)
+          }
+          searchHandler={searchHandler}
+          searchFieldStyle={`${
+            darkMode
+              ? dashboardStyles['search-input-dark']
+              : dashboardStyles['search-input-light']
+          }`}
+          buttonStyle={`${
+            darkMode
+              ? dashboardStyles['search-button-dark']
+              : dashboardStyles['search-button-light']
+          }`}
+        />
+      </div>
+      <SearchSuggestions
+        onClickHandler={suggestionsHandler}
+        suggestions={suggestions}
+        style={`${
+          darkMode
+            ? dashboardStyles['suggestions-dark']
+            : dashboardStyles['suggestions-light']
+        }`}
+      />
+    </>
+  );
+};
+export default SearchSection;
