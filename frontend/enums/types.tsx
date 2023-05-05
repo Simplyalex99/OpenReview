0ca9fefd523eb0023605655d2c21c0574a7df8d5
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-05 15:33:31 -0400

    add: frontend: enums

--- frontend/enums/types.tsx
+++ frontend/enums/types.tsx
@@ -1,6 +1,6 @@
-export const BASE_URL = 'http://localhost:5000/businesses';
+export const BASE_URL = 'http://127.0.0.1:5000/businesses';
 
-const URLTypesEnum = Object.freeze({
+const UrlApiTypesEnum = Object.freeze({
   AUTOCOMPLETE_URL: `${BASE_URL}/autocomplete`,
   SEARCH_BUSINESSES_URL: `${BASE_URL}/search`,
   POPULAR_RECOMMENDATION_URL: `${BASE_URL}/recommendations/popular`,
@@ -8,5 +8,9 @@ const URLTypesEnum = Object.freeze({
   BUSINESS_SUCCESS_URL: `${BASE_URL}/predictions/business-success`,
   REVIEW_TOPICS_URL: `${BASE_URL}/predictions/topics`,
 });
+export enum UrlPages {
+  DASHBOARD_SEARCH_PATH = '/dashboard/search',
+  HOME_PATH = '/',
+}
 
-export default URLTypesEnum;
+export default UrlApiTypesEnum;
