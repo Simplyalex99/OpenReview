4e1fef7b632f2cbc2db4960a242220812a59784b
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-16 16:56:36 -0400

    update:frontend: renamed Card prop

--- frontend/components/cards/Card.tsx
+++ frontend/components/cards/Card.tsx
@@ -3,11 +3,11 @@ import cardStyles from '../../styles/components/Card.module.scss';
 
 interface CardProps {
   children: ReactNode;
-  style?: string;
+  className?: string;
 }
 
-export const Card = ({ children, style }: CardProps) => {
-  return <div className={`${cardStyles.wrapper} ${style}`}>{children}</div>;
+export const Card = ({ children, className }: CardProps) => {
+  return <div className={`${cardStyles.wrapper} ${className}`}>{children}</div>;
 };
 
 export default Card;
