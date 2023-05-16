2661493b3a3584ad9b8d800b442116f99b66f4c2
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-16 16:59:15 -0400

    update: frontend: Modal component

--- frontend/components/others/Modal.tsx
+++ frontend/components/others/Modal.tsx
@@ -1,32 +1,48 @@
 import React, { useRef } from 'react';
-import { createPortal } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { useOnClickOutside } from '../../hooks';
+import modalStyles from '../../styles/components/Modal.module.scss';
+import { CloseSVG } from '../svg/common/Close';
+import { Card } from '../cards/Card';
 
 interface ModalProps {
   open: boolean;
   closeHandler: () => void;
   children: React.ReactNode;
   className?: string;
+  secondClassName?: string;
 }
 export const Modal = ({
   open,
   closeHandler,
   children,
   className,
+  secondClassName,
 }: ModalProps) => {
   if (typeof window === 'undefined' || !open) {
-    return;
+    return null;
   }
   const ref = useRef<HTMLDivElement | null>(null);
   useOnClickOutside(ref, closeHandler);
   // eslint-disable-next-line consistent-return
-  return createPortal(
-    <div>
-      <div ref={ref} className={className}>
-        <div>{children}</div>
-      </div>
-    </div>,
-    document.getElementById('modals') as Element
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <div className={modalStyles.container}>
+          <div ref={ref} className={`${modalStyles.wrapper} ${className}`}>
+            <Card
+              className={`${modalStyles['card-container']} ${secondClassName}`}
+            >
+              <div className={modalStyles.close}>
+                <CloseSVG onClick={closeHandler} />
+              </div>
+              <div>{children}</div>
+            </Card>{' '}
+          </div>
+        </div>,
+        document.getElementById('modal') as Element
+      )}
+    </>
   );
 };
 export default Modal;
