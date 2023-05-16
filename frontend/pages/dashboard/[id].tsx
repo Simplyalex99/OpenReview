f17e0de2987487b743ab6979e7eccce52245d942
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-16 17:32:20 -0400

    update: frontend: dashboard page
    ** description * : Added props and sections

--- frontend/pages/dashboard/[id].tsx
+++ frontend/pages/dashboard/[id].tsx
@@ -8,67 +8,38 @@ import {
   withSearch,
   HeadingSection,
   StatsSection,
+  CompetitorsSection,
+  RecommendationSection,
+  ReviewSection,
 } from '../../components';
 import { useAppSelector } from '../../hooks';
-import { fetcher, customFetcher } from '../../helpers';
+import { fetcher, customFetcher } from '../../utils';
 import UrlApiTypesEnum, { BASE_URL } from '../../enums/types';
+import {
+  ReviewTopicsProps,
+  CustomerReviewProps,
+  PopularBusinessProps,
+  BusinessCategoriesProps,
+  BusinessPredictionProps,
+  UserReviewProps,
+} from '../../typings/api.d';
+import { APIError } from '../../lib/exceptions';
 
-interface CustomerReviewsProps {
-  total: number;
-  reviews: [
-    {
-      id: string;
-      url: string;
-      text: string;
-      rating: number;
-      time_created: string;
-      user: {
-        id: string;
-        profile_url: string;
-        image_url: string;
-        name: string;
-      };
-    }
-  ];
-  possible_languages: Array<string>;
-}
-interface BusinessPredictionProps {
-  successful: boolean;
-  positive_reviews: number;
-  negative_reviews: number;
-  predictions: Array<boolean>;
-  status: number;
-}
-interface PopularBusinessProps {
-  businesses: [
-    {
-      business_id: string;
-      business_name: string;
-      business_rating: number;
-    }
-  ];
-}
-
-interface BusinessCategoriesProps {
-  categories: Array<string>;
-}
-type BusinessProps = {
-  customerReviews?: CustomerReviewsProps;
+export type BusinessProps = {
+  customerReviews?: UserReviewProps;
   popularBusinesses?: PopularBusinessProps;
   businessCategories?: BusinessCategoriesProps;
   businessPrediction?: BusinessPredictionProps;
   businessName?: string;
   businessAddress?: string;
 };
+
 const Dashboard: NextPageWithLayout = ({
   businessPrediction,
   businessName,
   businessAddress,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
   customerReviews,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
   popularBusinesses,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
   businessCategories,
 }: BusinessProps) => {
   const stateTheme = useAppSelector((state) => state.themeReducer);
@@ -91,61 +62,120 @@ const Dashboard: NextPageWithLayout = ({
   ) : (
     <NullComponent />
   );
+
+  const CompetitorsComponent = popularBusinesses ? (
+    <CompetitorsSection businesses={popularBusinesses.businesses} />
+  ) : (
+    <NullComponent />
+  );
+  const RecommendationComponent = businessCategories ? (
+    <RecommendationSection categories={businessCategories.categories} />
+  ) : (
+    <NullComponent />
+  );
+
+  let CustomerReviewsComponent = <NullComponent />;
+
+  if (customerReviews) {
+    CustomerReviewsComponent = (
+      <ReviewSection customerReviews={customerReviews} />
+    );
+  }
   return (
     <div className={dashboardStyles.wrapper}>
-      <section
-        className={`${dashboardStyles.heading} ${darkMode ? 'white' : 'black'}`}
-      >
-        {HeadingComponent}
-      </section>
-      <section className={dashboardStyles['grid-container']}>
-        {StatsComponent}
-      </section>
+      {HeadingComponent}
+
+      {StatsComponent}
+
+      {CompetitorsComponent}
+
+      {RecommendationComponent}
+      {CustomerReviewsComponent}
     </div>
   );
 };
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const businessID = context.params?.id ?? '';
-  const category = context.query?.category ?? '';
-  const businessName = context.query?.name ?? '';
-  const businessAddress = context.query?.address ?? '';
-  const {
-    REVIEW_TOPICS_URL,
-    CATEGORIES_RECOMMENDATION_URL,
-    POPULAR_RECOMMENDATION_URL,
-    BUSINESS_SUCCESS_URL,
-  } = UrlApiTypesEnum;
-  const httpMethod = 'POST';
-  const token = undefined;
-  const endpoint1 = BASE_URL + `/${businessID}/reviews`;
-  const customerReviewsJSON = await fetcher(endpoint1);
-  const reviewTopicsData = await customFetcher(
-    REVIEW_TOPICS_URL,
-    token,
-    httpMethod,
-    customerReviewsJSON
-  );
-  const endpoint2 = `${POPULAR_RECOMMENDATION_URL}?category=${category}`;
-  const popularBusinessesData = await fetcher(endpoint2);
-  const endpoint3 = `${CATEGORIES_RECOMMENDATION_URL}?category=${category}`;
-  const businessCategoriesData = await fetcher(endpoint3);
-  const businessPredictionData = await customFetcher(
-    BUSINESS_SUCCESS_URL,
-    token,
-    httpMethod,
-    customerReviewsJSON
-  );
+  try {
+    const businessID = context.params?.id ?? '';
+    const category = context.query?.category ?? '';
+    const businessName = context.query?.name ?? '';
+    const businessAddress = context.query?.address ?? '';
+    const {
+      REVIEW_TOPICS_URL,
+      CATEGORIES_RECOMMENDATION_URL,
+      POPULAR_RECOMMENDATION_URL,
+      BUSINESS_SUCCESS_URL,
+    } = UrlApiTypesEnum;
 
-  return {
-    props: {
-      customerReviews: reviewTopicsData,
-      popularBusinesses: popularBusinessesData,
-      businessCategories: businessCategoriesData,
-      businessPrediction: businessPredictionData,
-      businessName,
-      businessAddress,
-    },
-  };
+    const httpMethod = 'POST';
+    const token = undefined;
+    const endpoint1 = BASE_URL + `/${businessID}/reviews`;
+    const customerReviewResponse = await fetcher(endpoint1);
+    let customerReviewData = undefined;
+    let customerReviewsJSON = {};
+    if (customerReviewResponse) {
+      customerReviewsJSON = customerReviewResponse;
+      customerReviewData = customerReviewsJSON as CustomerReviewProps;
+    }
+
+    const reviewTopicResponse = await customFetcher(
+      REVIEW_TOPICS_URL,
+      token,
+      httpMethod,
+      customerReviewResponse
+    );
+    let reviewTopicData: any | undefined = undefined;
+    if (reviewTopicResponse) {
+      const reviewTopicJSON = reviewTopicResponse;
+      reviewTopicData = reviewTopicJSON as ReviewTopicsProps;
+    }
+
+    const endpoint2 = `${POPULAR_RECOMMENDATION_URL}?category=${category}`;
+    const popularBusinessesData = await fetcher(endpoint2);
+    const endpoint3 = `${CATEGORIES_RECOMMENDATION_URL}?category=${category}`;
+    const businessCategoriesData = await fetcher(endpoint3);
+    const businessPredictionResponse = await customFetcher(
+      BUSINESS_SUCCESS_URL,
+      token,
+      httpMethod,
+      customerReviewResponse
+    );
+
+    const reviews = customerReviewData?.reviews;
+    let mergedCustomerReviews = undefined;
+
+    if (reviews) {
+      const mergedCustomerData = reviews?.map((review, index) => {
+        const { predictions } = businessPredictionResponse;
+
+        const { topics } = reviewTopicData;
+
+        const topicCategories = topics[index].categories;
+        const isSuccesful = predictions[index];
+        const sentiment = isSuccesful ? 'Positive' : 'Negative';
+        const categories = [sentiment, ...topicCategories];
+
+        return { ...review, categories };
+      });
+      mergedCustomerReviews = {
+        ...customerReviewData,
+        reviews: mergedCustomerData,
+      };
+    }
+
+    return {
+      props: {
+        customerReviews: mergedCustomerReviews,
+        popularBusinesses: popularBusinessesData,
+        businessCategories: businessCategoriesData,
+        businessPredictionResponse,
+        businessName,
+        businessAddress,
+      },
+    };
+  } catch (err: any) {
+    throw new APIError();
+  }
 };
 
 export const DashboardPage = withSearch(Dashboard) as NextPageWithLayout;
