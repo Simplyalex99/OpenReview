cc34410d8c88031d20bcb2789d2736b536ffa447
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-16 17:02:10 -0400

    update: frontend: moved functions to utils

--- frontend/helpers/fetcher.tsx
+++
@@ -1,8 +0,0 @@
-export const fetcher = (url: string, token?: string) =>
-  fetch(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-  }).then((res) => res.json());
-export default fetcher;
