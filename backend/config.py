d0edbfe6424bf5beeab3674c00ac2996ad19a059
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-24 20:30:33 -0400

    add: backend:  print debug

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 class BaseConfig(object):
     CACHE_TYPE = os.environ['CACHE_TYPE']
+    print("cache type;{}".format(CACHE_TYPE))
     CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST']
     CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT']
     CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB']
