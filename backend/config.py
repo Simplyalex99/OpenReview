486b368d3319d68a5e7f450e44674dd4d8f4c609
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-24 21:19:13 -0400

    update: backend: config file

--- backend/config.py
+++ backend/config.py
@@ -2,10 +2,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 class BaseConfig(object):
-    CACHE_TYPE = os.environ['CACHE_TYPE']
-    print("cache type;{}".format(CACHE_TYPE))
-    CACHE_REDIS_HOST = os.environ['CACHE_REDIS_HOST']
-    CACHE_REDIS_PORT = os.environ['CACHE_REDIS_PORT']
-    CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB']
+    CACHE_TYPE = "redis"
+    CACHE_REDIS_HOST = "redis"
+    CACHE_REDIS_PORT = 6379
+    CACHE_REDIS_DB = 0
     CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
-    CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
+    CACHE_DEFAULT_TIMEOUT = 500
